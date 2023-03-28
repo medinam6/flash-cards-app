fa0f66e4c876603ea6cc48d2b07b5b8431e5191b
Meyvi <meyvim@gmail.com>
2023-03-28 10:13:19 -0700

    Set up server and connec to db

---
+++ my-flash-cards-app/backend/src/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import {db, connectToDb} from './db.js'
+
+const app = express();
+app.use(express.json());
+
+app.get('/api/cards/', async (req, res) => {
+
+    const cards = await db.collection('cards').findOne({ question: 'selector' });
+    if (cards) {
+        res.send(cards);
+    } else {
+        res.sendStatus(404);
+    }
+    
+});
+
+connectToDb(() => {
+    console.log('Successfully connected to database');
+
+    app.listen(8000, () => {
+    console.log('Server is listening on port 8000');
+    });
+});
