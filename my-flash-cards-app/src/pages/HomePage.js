9b6314e33524e0e8241d4bd99b03c15bf8d865c8
Meyvi <meyvim@gmail.com>
2023-04-05 16:33:08 -0700

    Add card from mongodb to front end

--- my-flash-cards-app/src/pages/HomePage.js
+++ my-flash-cards-app/src/pages/HomePage.js
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-const HomePage = () => (
+const HomePage = () => {
+
+    const [cardsData, setCardsData] = useState();
+
+    useEffect(() => {
+        const getCards = async () => {
+            const response = await axios.get('/api/cards/');
+            const newCardData = response.data;
+            setCardsData(newCardData);
+        };
+
+        getCards();
+    }, []);
+
+    if (!cardsData) {
+        return null;
+    }
+
+    return (
     <>
         <h1>Flash Cards</h1>
         <div>
-            <h2>56 Cards</h2>
+            <h2>{cardsData.length} Cards</h2>
             <div>
-                <h5>CSS</h5>
-                <p>Acronym for Cascading Style Sheet. Defines how to display HTML elements.</p>
+                <h5>{cardsData[0].question}</h5>
+                <p>{cardsData[0].answer}</p>
             </div>
-            <div>
-                <h5>selector</h5>
-                <p>Specify which elements are to be influenced by the style rule</p>
-            </div>
+
         </div>
     </>
-)
+    );
+};
 
 export default HomePage;
