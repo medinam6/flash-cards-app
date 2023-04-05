36cda80627f6d3432c22d4f6c851aee79d9d04a4
Meyvi <meyvim@gmail.com>
2023-04-05 15:02:38 -0700

    Add mongodb atlast to server

--- my-flash-cards-app/backend/src/db.js
+++ my-flash-cards-app/backend/src/db.js
@@ -1,9 +1,12 @@
 import { MongoClient } from 'mongodb';
+import 'dotenv/config';
 
 let db;
 
 async function connectToDb(cb) {
-    const client = new MongoClient('mongodb://127.0.0.1:27017');
+    const client = new MongoClient(
+        `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster36071.h1fsoh4.mongodb.net/?retryWrites=true&w=majority`
+        );
     await client.connect();
 
     db = client.db('my-flash-cards-db');
--- my-flash-cards-app/backend/src/server.js
+++ my-flash-cards-app/backend/src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import {db, connectToDb} from './db.js'
+import 'dotenv/config';
 
 const app = express();
 app.use(express.json());
