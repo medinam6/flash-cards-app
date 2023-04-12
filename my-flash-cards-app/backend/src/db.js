fe8d4dd8a51aa5240febc3c2477daab53964e93f
Meyvi <meyvim@gmail.com>
2023-04-11 17:02:34 -0700

    Updated db

--- my-flash-cards-app/backend/src/db.js
+++ my-flash-cards-app/backend/src/db.js
@@ -5,8 +5,8 @@ let db;
 
 async function connectToDb(cb) {
     const client = new MongoClient(
-        `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster36071.h1fsoh4.mongodb.net/?retryWrites=true&w=majority`
-        );
+       `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@flash-cards.c5hbulq.mongodb.net/?retryWrites=true&w=majority`
+       );
     await client.connect();
 
     db = client.db('my-flash-cards-db');
